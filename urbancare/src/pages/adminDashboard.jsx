44f46a7030940b8af7dcf9b477bd66182cecef61
Ritik Rawat <rithikrawat2004@gmail.com>
2026-01-29 02:04:06 +0530

    Create adminDashboard.jsx

---
+++ urbancare/src/pages/adminDashboard.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class adminDashboard extends Component {
+  render() {
+    return (
+      <div>adminDashboard</div>
+    )
+  }
+}
+
+export default adminDashboard
