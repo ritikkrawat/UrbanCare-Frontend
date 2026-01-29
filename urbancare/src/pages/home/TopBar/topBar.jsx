6bcee669a1ed89b9a5f474f46523d6bd4fdf3b16
Ritik Rawat <rithikrawat2004@gmail.com>
2026-01-30 02:10:04 +0530

    Create topBar.jsx

---
+++ urbancare/src/pages/home/TopBar/topBar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './topBar.css';
+
+const Topbar = () => {
+  return (
+    <div className="topbar">
+      {/* Left side */}
+      <div className="topbar-left">
+        <span>Government of India</span>
+        <span className="divider">|</span>
+        <span>Public Grievance & Civic Services</span>
+      </div>
+
+      {/* Right side */}
+      <div className="topbar-right">
+        <a href="/">Home</a>
+        <a href="#">Contact Us</a>
+        <a href="#">About Us</a>
+        <a href="#">FAQs/Help</a>
+        {/* <a href="#">Site Map</a> */}
+      </div>
+    </div>
+  );
+};
+
+export default Topbar;
