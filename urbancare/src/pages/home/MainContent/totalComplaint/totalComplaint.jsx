91f2aa60ca27d93bc8f8409daf82ba4f1f5eb5a6
Ritik Rawat <rithikrawat2004@gmail.com>
2026-01-30 02:09:51 +0530

    Create totalComplaint.jsx

---
+++ urbancare/src/pages/home/MainContent/totalComplaint/totalComplaint.jsx
@@ -0,0 +1,9 @@
+import './totalComplaint.css'
+
+const totalComplaint = () => {
+  return (
+    <div className='total-complaint-section'>totalComplaint</div>
+  )
+}
+
+export default totalComplaint
