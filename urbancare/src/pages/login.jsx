bde1d00f1de0573c5746bf49f9f8f72c8672ccf8
Ritik Rawat <rithikrawat2004@gmail.com>
2026-01-30 22:20:01 +0530

    Update login.jsx

--- urbancare/src/pages/login.jsx
+++ urbancare/src/pages/login.jsx
@@ -1,55 +1,19 @@
-import { Link, useNavigate } from "react-router-dom";
+import Topbar from "./home/TopBar/topBar.jsx";
+import Head from "./home/Head/head.jsx";
+import MainNavbar from "./home/MainNavbar/mainNavbar.jsx";
+import MainContent from "./home/MainContent/mainContent.jsx";
+import Footer from './home/Footer/footer.jsx';
 
-const Login = () => {
-  const navigate = useNavigate();
-
-  const handleLogin = (e) => {
-    e.preventDefault();
-    // UI only: navigate directly
-    navigate("/home");
-  };
-
+const login = () => {
   return (
-    <div className="auth-container">
-      <div className="auth-card">
-        {/* Logo / App Name */}
-        <div className="auth-header">
-          <h2>UrbanCare</h2>
-          <p>Civic Issue Reporting System</p>
-        </div>
-
-        {/* Login Form */}
-        <form onSubmit={handleLogin}>
-          <div className="form-group">
-            <label>
-              Username <span className="required">*</span>
-            </label>
-            <input type="text" placeholder="Enter username" />
-          </div>
-
-          <div className="form-group">
-            <label>
-              Password <span className="required">*</span>
-            </label>
-            <input type="password" placeholder="Enter password" />
-          </div>
-
-          <button type="submit" className="primary-btn">
-            Log in
-          </button>
-          
-        </form>
-
-        {/* Footer Link */}
-        <div className="auth-footer">
-          <p>
-            Don’t have an account?{" "}
-            <Link to="/register">Register</Link>
-          </p>
-        </div>
-      </div>
-    </div>
+    <>
+      <Topbar />
+      <Head />
+      <MainNavbar />
+      <MainContent />
+      <Footer />
+    </>
   );
 };
 
-export default Login;
+export default login;
