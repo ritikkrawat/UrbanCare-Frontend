b841b14f8a4342f67bc4dae4e554db69f8ab7f65
Ritik Rawat <rithikrawat2004@gmail.com>
2026-01-30 22:19:55 +0530

    Update boxSection.jsx

--- urbancare/src/pages/home/MainContent/boxSection/boxSection.jsx
+++ urbancare/src/pages/home/MainContent/boxSection/boxSection.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import "./boxSection.css";
+import registerImage from "../../../../assets/register.png";
+import statusImage from "../../../../assets/status.png";
+import contactImage from "../../../../assets/contact.png";
 
-const boxSection = () => {
+const BoxSection = () => {
   return (
-    <div>boxSection</div>
-  )
-}
+    <div className="box-section">
+      <div className="box-card blue">
+        <div className="box-icon">
+          <img src={registerImage} alt="Register or Login" />
+        </div>
+        <button className="box-btn">REGISTER / LOGIN</button>
+      </div>
 
-export default boxSection
+      <div className="box-card pink">
+        <div className="box-icon">
+          <img src={statusImage} alt="View Status" />
+        </div>
+        <button className="box-btn">VIEW STATUS</button>
+      </div>
+
+      <div className="box-card yellow">
+        <div className="box-icon">
+          <img src={contactImage} alt="Contact Us" />
+        </div>
+        <button className="box-btn">CONTACT US</button>
+      </div>
+    </div>
+  );
+};
+
+export default BoxSection;
