d48f3f1cabd0df3b9fd6a72589d2d985ed6f82a0
Ritik Rawat <rithikrawat2004@gmail.com>
2026-01-31 21:48:52 +0530

    Update boxSection.jsx

--- urbancare/src/pages/home/MainContent/boxSection/boxSection.jsx
+++ urbancare/src/pages/home/MainContent/boxSection/boxSection.jsx
@@ -2,15 +2,22 @@ import "./boxSection.css";
 import registerImage from "../../../../assets/register.png";
 import statusImage from "../../../../assets/status.png";
 import contactImage from "../../../../assets/contact.png";
+import { useNavigate } from "react-router-dom";
 
 const BoxSection = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="box-section">
       <div className="box-card blue">
         <div className="box-icon">
           <img src={registerImage} alt="Register or Login" />
         </div>
-        <button className="box-btn">REGISTER / LOGIN</button>
+        <button className="box-btn" 
+          onClick={() => navigate("/login")}
+        > 
+          REGISTER / LOGIN
+        </button>
       </div>
 
       <div className="box-card pink">
