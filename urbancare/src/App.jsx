feac56bb7a8984908645e5e78917caa22dd6b174
Ritik Rawat <rithikrawat2004@gmail.com>
2026-03-23 15:10:03 +0530

    Update App.jsx

--- urbancare/src/App.jsx
+++ urbancare/src/App.jsx
@@ -2,14 +2,26 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/home";
 import Login from "./pages/login";
 import Register from "./pages/register";
+import Dashboard from "./pages/userDashboard";
+import ProtectedRoute from "./routes/protectedRoute.jsx";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
