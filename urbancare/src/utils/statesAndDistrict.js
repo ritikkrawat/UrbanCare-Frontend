28b55d853edeb6965fad168ffcb85f71bff2e175
Ritik Rawat <rithikrawat2004@gmail.com>
2026-03-23 15:10:28 +0530

    Create statesAndDistrict.js

---
+++ urbancare/src/utils/statesAndDistrict.js
@@ -0,0 +1,20 @@
+export const statesData = {  
+   "states":[  
+      {  
+         "state":"Delhi (NCT)",
+         "districts":[  
+            "Central Delhi",
+            "East Delhi",
+            "New Delhi",
+            "North Delhi",
+            "North East  Delhi",
+            "North West  Delhi",
+            "Shahdara",
+            "South Delhi",
+            "South East Delhi",
+            "South West  Delhi",
+            "West Delhi"
+         ]
+      }
+   ]
+}
